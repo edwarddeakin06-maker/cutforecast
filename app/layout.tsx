ef2fa612771d09d3ee6f489de831ec02264a70bf
Edward <edwarddeakin06@gmail.com>
2026-02-28 21:12:42 +0000

    fix adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,17 +19,28 @@ export const metadata = {
     "Plan your fat loss visually. Calculate calories, track your progress, and forecast your weight loss with CutForecast.",
 };
 
+
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1830663667907565"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
         {children}
+
       </body>
     </html>
   );
